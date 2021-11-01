94f6a2e337a6b006f028a79eddd21429ff9b6796
pablo-blancoc <pablo_blancoc@outlook.es>
2021-11-01 12:56:21 -0600

    added another library

--- delaunay.cpp
+++ delaunay.cpp
@@ -6,6 +6,7 @@
 //SudoLeo 2010/7/20
 //CGAL required, GLUT required
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 //#include <CGAL/Triangulation_euclidean_traits_xy_3.h>
